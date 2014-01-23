125ba1b98f10702173978db0e27826a542b7293f
sora mame <ikutake04.12.15@gmail.com>
2014-01-23 13:14:23 +0900

    ok

--- HelloGitHub/ViewController.h
+++ HelloGitHub/ViewController.h
@@ -8,6 +8,41 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController{
+
+    BOOL startInput;
+    int currentValue;
+    int operation;
+//IBOutlet UIButton *dot;
+    IBOutlet UIButton *zero;
+//IBOutlet UIButton *wzero;
+    IBOutlet UIButton *one;
+    IBOutlet UIButton *two;
+    IBOutlet UIButton *three;
+    IBOutlet UIButton *four;
+    IBOutlet UIButton *five;
+    IBOutlet UIButton *six;
+    IBOutlet UIButton *seven;
+    IBOutlet UIButton *eight;
+    IBOutlet UIButton *nine;
+    IBOutlet UIButton *equal;
+    IBOutlet UIButton *add;
+    IBOutlet UIButton *sub;
+    IBOutlet UIButton *div;
+    IBOutlet UIButton *mul;
+    IBOutlet UIButton *plumi;
+//IBOutlet UIButton *rem;
+//IBOutlet UIButton *ans;
+    IBOutlet UIButton *clear;
+    IBOutlet UITextField *result;
+}
+
+- (IBAction)number_Push:(id)sender;
+- (IBAction)clear_Push:(id)sender;
+- (IBAction)equal_Push:(id)sender;
+- (IBAction)op_Push:(id)sender;
+- (IBAction)del_push:(id)sender;
+- (IBAction)plumi_push:(id)sender;
+
 
 @end
--- HelloGitHub/ViewController.m
+++ HelloGitHub/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    startInput = YES;
+    currentValue = 0;
 }
 
 - (void)didReceiveMemoryWarning
@@ -26,4 +28,130 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+
+int a,c,d;
+
+- (IBAction)number_Push:(id)sender {
+    UIButton *b = (UIButton *)sender;
+    
+    if( startInput ){
+        if( b.tag == 0 ){
+            if(i >= 1){
+                l = 0;
+                b.tag = 0;
+                i = 0;
+            }
+            else{
+                l = 1;
+                return;
+            }
+        }
+        // 新しく表示する文字列を作成
+        
+        result.text = [NSString stringWithFormat:@"%d", b.tag];
+        startInput =  NO;
+    }
+    else {
+        // すでに表示されている文字列に連結
+        result.text = [NSString stringWithFormat:@"%@%d", result.text, b.tag];
+        
+    }
+    add.hidden = NO;
+    sub.hidden = NO;
+    div.hidden = NO;
+    mul.hidden = NO;
+    equal.hidden = NO;
+    plumi.hidden = NO;
+}
+
+- (IBAction)clear_Push:(id)sender {
+    i = 0;
+    result.text = @"0";
+    startInput = YES;
+    add.hidden = NO;
+    sub.hidden = NO;
+    div.hidden = NO;
+    mul.hidden = NO;
+    equal.hidden = NO;
+    plumi.hidden = NO;
+}
+
+int i,j;
+int l;
+
+- (IBAction)equal_Push:(id)sender {
+    // 直前に押された演算子で場合分け
+    i = 0;
+    if( operation == 1 ){
+        currentValue += [result.text intValue];
+        operation = 0;
+    }
+    else if( operation == 2 ){
+        currentValue -= [result.text intValue];
+        operation = 0;
+    }
+    else if( operation == 3 ){
+        currentValue *= [result.text intValue];
+        operation = 0;
+    }
+    else if( operation == 4 ){
+        currentValue /= [result.text intValue];
+        operation = 0;
+    }
+    
+        // 表示の更新
+    result.text = [NSString stringWithFormat:@"%d", currentValue];
+    startInput = YES;
+    add.hidden = NO;
+    sub.hidden = NO;
+    div.hidden = NO;
+    mul.hidden = NO;
+    equal.hidden = NO;
+    plumi.hidden = NO;
+}
+
+- (IBAction)op_Push:(id)sender {
+    
+    i++;
+    if( i >= 2){
+        if( operation == 1 ){
+            currentValue += [result.text intValue];
+        } else if( operation == 2 ){
+            currentValue -= [result.text intValue];
+        }
+        else if( operation == 3 ){
+            currentValue *= [result.text intValue];
+        }
+        else if( operation == 4 ){
+            currentValue /= [result.text intValue];
+        }
+    
+        // 表示の更新
+        result.text = [NSString stringWithFormat:@"%d", currentValue];
+        startInput = YES;
+    }
+    UIButton *b = (UIButton *)sender;
+    
+    // 現在値の保存
+    currentValue = [result.text intValue];
+    
+    // 演算の保存
+    operation = b.tag;
+    
+    startInput = YES;
+   
+    add.hidden = YES;
+    sub.hidden = YES;
+    div.hidden = YES;
+    mul.hidden = YES;
+    equal.hidden = YES;
+    
+}
+
+- (IBAction)plumi_push:(id)sender {
+    currentValue = [result.text intValue];
+    currentValue = -1*currentValue;
+    result.text = [NSString stringWithFormat:@"%d", currentValue];
+}
 @end
